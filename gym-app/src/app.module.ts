e78bd3cbe8ea86946e215a2687fa0b00b11f1392
Aiden Campbell <Campbell.e.aiden@gmail.com>
2023-03-08 12:59:27 +0000

    planetscale

--- gym-app/src/app.module.ts
+++ gym-app/src/app.module.ts
@@ -3,26 +3,37 @@ import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { getEnvPath } from './common/helper/env.helper';
 import { APP_GUARD } from '@nestjs/core';
 import { RolesGuard } from './auth/guards/roles.guard';
 
 const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
-console.log('using envFilePath :', `${envFilePath}
-To change env file, set NODE_ENV to one of the following: development, production`);
+console.log(
+  'using envFilePath :',
+  `${envFilePath}
+To change env file, set NODE_ENV to one of the following: development, production`,
+);
 
 @Module({
   imports: [
     TasksModule,
     ConfigModule.forRoot({ envFilePath, isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'gymAppDb',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-      autoLoadEntities: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        database: 'gymApp',
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        autoLoadEntities: true,
+        ssl: {
+          ca: configService.get('DB_SSL'),
+        },
+        url: configService.get('DB_URL'),
+      }),
+      inject: [ConfigService],
     }),
     AuthModule,
     UsersModule,
