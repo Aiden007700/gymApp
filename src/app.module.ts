7e358446a44a0dd83e341515f656cc777f2a3241
Aiden Campbell <Campbell.e.aiden@gmail.com>
2023-03-16 15:44:43 +0000

    migration suport added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,42 +3,33 @@ import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { getEnvPath } from './common/helper/env.helper';
 import { APP_GUARD } from '@nestjs/core';
 import { RolesGuard } from './auth/guards/roles.guard';
 import { AppController } from './app/app.controller';
+import { ExerciseModule } from './exercise/exercise.module';
 import * as fs from 'fs';
+import { typeOrmAsyncConfig } from './common/config/typeorm.config';
 
 const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
+console.log(process.env.NODE_ENV);
 console.log(
   'using envFilePath :',
   `${envFilePath}
 To change env file, set NODE_ENV to one of the following: development, production`,
 );
 
+
 @Module({
   imports: [
     TasksModule,
-    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        database: 'gymApp',
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: false,
-        autoLoadEntities: true,
-        ssl: {
-          ca: fs.readFileSync(process.env.DB_SSL || configService.get('DB_SSL')),
-        },
-        url: process.env.DB_URL || configService.get('DB_URL'),
-      }),
-      inject: [ConfigService],
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
     AuthModule,
     UsersModule,
+    ExerciseModule,
   ],
   providers: [
     {
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,5 @@
-import { Role } from "src/auth/enums/role.enum";
-import { Task } from "src/tasks/entities/task.entity";
+import { Role } from "../../auth/enums/role.enum";
+import { Task } from "../../tasks/entities/task.entity";
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn, Unique } from "typeorm";
 
 @Entity()
@@ -19,6 +19,6 @@ export class User {
   @OneToMany(() => Task, task => task.user, { eager: true })
   tasks: Task[];
 
-  // @Column('enum', { enum: Role, array: true, default: [Role.User] })
-  // roles: Role[];
+  @Column('enum', { enum: Role, array: true, default: [Role.User] })
+  roles: Role[];
 }
