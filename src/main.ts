26478d9e19533dcd83ad0df205fe5601472ff196
Aiden Campbell <Campbell.e.aiden@gmail.com>
2023-03-17 08:40:18 +0000

    swagger wip

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { swaggerConfig } from './common/config/swagger.config';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 async function bootstrap() {
@@ -11,6 +13,7 @@ async function bootstrap() {
     }),
   );
   app.useGlobalInterceptors(new TransformInterceptor());
+  SwaggerModule.setup('api', app, SwaggerModule.createDocument(app, swaggerConfig));
   await app.listen(process.env.PORT || 3000);
 }
 
--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -12,25 +12,38 @@ import {
 } from '@nestjs/common';
 import { GetUser } from 'src/modules/auth/decorators/get-user.decorator';
 import { JwtAuthGuard } from 'src/modules/auth/guards/jwt-auth.guard';
-import RequestWithUser from 'src/common/types/requestWithUser.interface';
 import { User } from 'src/modules/users/entities/user.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { Task } from './entities/task.entity';
 import { TasksService } from './tasks.service';
+import {
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiBadRequestResponse
+} from '@nestjs/swagger';
 
 @Controller('tasks')
+@ApiTags('tasks')
 @UseGuards(JwtAuthGuard)
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get('/:id')
+  @ApiOperation({ summary: 'Get task by id' })
+  @ApiResponse({ status: 200, description: 'The found record', type: Task })
   getTaskById(@Param('id') id: number): Promise<Task> {
     return this.tasksService.getTaskBy(id);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all tasks' })
+  @ApiResponse({ status: 200, description: 'The found records', type: [Task] })
+  @ApiBody({ type: GetTasksFilterDto })
   getTasks(
     @Query() filter: GetTasksFilterDto,
     @GetUser() user: User,
@@ -39,6 +52,11 @@ export class TasksController {
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create a task' })
+  @ApiResponse({ status: 201, description: 'The record has been successfully created.', type: Task })
+  @ApiCreatedResponse({ description: 'The record has been successfully created.', type: Task })
+  @ApiBadRequestResponse({ description: 'Bad request' })
+  @ApiBody({ type: CreateTaskDto })
   createTask(
     @Body() createTaskDto: CreateTaskDto,
     @GetUser() user: User,
@@ -47,6 +65,8 @@ export class TasksController {
   }
 
   @Patch('/:id/status')
+  @ApiOperation({ summary: 'Update task status' })
+  @ApiResponse({ status: 200, description: 'The record has been successfully updated.', type: Task })
   updateTaskStatus(
     @Param('id') id: number,
     @Body() updateTaskStatusDto: UpdateTaskStatusDto,
@@ -55,6 +75,8 @@ export class TasksController {
   }
 
   @Delete('/:id')
+  @ApiOperation({ summary: 'Delete task' })
+  @ApiResponse({ status: 200, description: 'The record has been successfully deleted.', type: Task })
   deleteTask(@Param('id') id: number): Promise<Task> {
     return this.tasksService.deleteTask(id);
   }
