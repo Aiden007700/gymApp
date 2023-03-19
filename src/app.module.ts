540a892a8050a5397fafb32d0cf133e296a2ea97
Aiden Campbell <Campbell.e.aiden@gmail.com>
2023-03-19 19:30:07 +0000

    refactor

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { ConfigModule } from '@nestjs/config';
-import { APP_GUARD } from '@nestjs/core';
-import { RolesGuard } from './modules/auth/guards/roles.guard';
 import { AppController } from './modules/app/app.controller';
 import { ExerciseModule } from './modules/exercise/exercise.module';
 import { typeOrmAsyncConfig } from './common/config/typeorm.config';
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { Roles } from 'src/modules/auth/decorators/roles.decorator';
-import { Role } from 'src/modules/auth/enums/role.enum';
+import { Roles } from '../../modules/auth/decorators/roles.decorator';
+import { Role } from '../../modules/auth/enums/role.enum';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -9,10 +12,25 @@ import { UsersService } from './users.service';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-    //delete this later
+
     @Get()
+    @UseGuards(JwtAuthGuard, RolesGuard)
     @Roles(Role.Admin)
     async get() {
       return this.usersService.find()
+    }
+
+    @Get(':email')
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.Admin)
+    async getOne(email: string) {
+      return this.usersService.findUserByEmail(email)
+    }
+
+    @Post()
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.Admin)
+    async create(@Body() createUserDto: CreateUserDto) {
+      return this.usersService.create(createUserDto)
     }
 }
